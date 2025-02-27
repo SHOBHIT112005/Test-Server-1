e5a10a3dd850fbdfbe416ac9b764de2a9c9a37f0
SHOBHIT112005 <ironmannox112005@gmail.com>
2025-02-28 02:22:56 +0530

    1

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 
-const app = express();//just like we have .pi, .random, etc for MATH function so that we have many for app function
+const app = express(); //just like we have .pi, .random, etc for MATH function so that we have many for app function
 
-const port = 3000;//for the servver to listen
+const port = 3000; //for the servver to listen
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/twitter",(req,res) => {
-    res.send("Hello Twitter!");
-})
+app.get("/twitter", (req, res) => {
+  res.send("Hello Twitter!");
+});
 
-app.get("/login" , (req,res) => {
-    res.send("<h1>Please Login via valid Creds</h1>");
-})
+app.get("/login", (req, res) => {
+  res.send("<h1>Please Login via valid Creds</h1>");
+});
 
-app.get("/youtube",(req,res) => {
-    res.send("<h2>Chai aur Code</h2>");
-})
+app.get("/youtube", (req, res) => {
+  res.send("<h2>Chai aur Code</h2>");
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
